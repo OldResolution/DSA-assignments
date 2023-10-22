8209b2547bab2a469f947886530edd8fef3884cd
OldResolution <140333635+OldResolution@users.noreply.github.com>
2023-10-23 00:31:01 +0530

    Update Savin52_23.c

--- Savin52_23.c
+++ Savin52_23.c
@@ -5,41 +5,67 @@ Author Name:Savin Johnson
 Date:2/09/23
 Description:Program to implement Merge Sort 
 */
-#include <stdio.h>
-
-void insertionSort(int arr[], int n) {
-    int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
-
-        // Move elements of arr[0..i-1] that are greater than key
-        // to one position ahead of their current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
+#include<stdio.h>
+#include<stdlib.h>
+void partition(int a[], int low, int high);
+void main(){
+    int n,temp,i,j,a[20];
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Enter Elements:");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("\nBefore Sorting:");
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    partition(a,0,n-1);
+    printf("\nAfter Sorting:");
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
     }
 }
-
-int main() {
-    int arr[] = {12, 11, 13, 5, 6};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Unsorted array: \n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+void partition(int a[], int low, int high){
+    int mid=0;
+    if(low<high){                   //divide the array into half till it has single element
+        mid=(low+high)/2;
+        partition(a,low,mid);
+        partition(a,mid+1,high);
+        mergesort(a,low,mid,high);
     }
-    printf("\n");
-
-    insertionSort(arr, n);
-
-    printf("Sorted array: \n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+}
+mergesort(int a[],int low, int mid, int high){
+    int i,m,k,l,temp[20];
+    l=low;
+    i=low;                          //iterator for merged array
+    m=mid+1;
+    while(l<=mid && m<=high){       //this will iterate till all the element from left and right subarray are compared.
+        if(a[l]<=a[m]){             //a[l] points to first element of left sub array,a[m] points to first element of right sub array
+            temp[i]=a[l];           //temp is merged array
+            l++;
+            i++;
+        }
+        else{
+            temp[i]=a[m];
+            m++;
+            i++;
+            
+        }
+    }//end of while
+    if(l>mid){
+        for(k=m;k<=high;k++){
+            temp[i]=a[k];
+            i++;
+        }
     }
-    printf("\n");
-
-    return 0;
+    else{
+        for(k=l;k<=mid;k++){
+            temp[i]=a[k];
+            i++;
+        }
+    }
+    for(k=low;k<=high;k++){
+        a[k]=temp[k];
+    }
 }
